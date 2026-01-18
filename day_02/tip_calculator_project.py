ac62789811b4d05f47d55d274fb4966434f8277f
Yatharth Sharma <syatharth2212@gmail.com>
2026-01-19 00:09:01 +0530

    Added Day 2 project – Tip Calculator

---
+++ day_02/tip_calculator_project.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator!")
+Total_bill = int(input('What was the total bill? $ '))
+tip = int(input("How much tip would you like to give? $"))
+people = int(input("How many people to split the bill? "))
+final_bill = ((Total_bill * (1 + (tip/100))) / people)
+amount = round(final_bill, 2)
+print(f"Each person should pay: ${final_bill}")
